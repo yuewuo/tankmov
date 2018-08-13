bfbd903e6df849bccb42bfd7d2163e1a2f1e083c
wuyuepku <wuyue16@pku.edu.cn>
2018-08-13 11:18:53 -0500

    archive vue project and later delete them
    because vue with webpack is too large for this project, I will move to a simple vue version later

--- vueproj/src/App.vue
+++ vueproj/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-header><img src="./assets/tank.png"><font>tankmov controller</font></el-header>
+      <el-main>
+        <el-button v-on:click="{{ setspeed }}">set speed</el-button>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -16,8 +20,33 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
+
+.el-header {
+  font-family: 'Ubuntu', sans-serif;
+  background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-header img {
+  width: 90px;
+  margin-bottom: -20px;
+  margin-left: -15px;
+}
+
+.el-header font {
+  margin-left: 20px;
+  font-size: 22px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  height: 400px;
+}
+
 </style>
--- vueproj/src/main.js
+++ vueproj/src/main.js
@@ -2,14 +2,40 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(VueResource)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  data: {
+    setspeed: function () {
+      
+    }
+  },
+  methods: {
+    setspeed: function (event) {
+      if (event) {
+        alert(event.target.tagName)
+      }
+    }
+  }
+})
+
+let actions = {
+  setSpeed: {method: 'POST', url: '/api/speed/'}
+}
+var esp32 = Vue.resource('', {}, actions)
+
+esp32.setSpeed({speed: 100}).then(response => {
+  // success callback
+}, response => {
+  // error callback
 })
