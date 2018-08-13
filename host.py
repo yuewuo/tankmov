c374e8c40dd5db3c767705c64b950aa5b659ace8
wuyuepku <wuyue16@pku.edu.cn>
2018-08-13 11:29:38 -0500

    adding arduino project & python API simulator

---
+++ host.py
@@ -0,0 +1,5 @@
+"""
+this file is for testing the index.html file, with server side API simulation
+"""
+
+
---
+++ movduino/movduino.ino
@@ -0,0 +1,23 @@
+/*
+ * movduino.ino
+ * 
+ * Created on 2018/6/13, wuyuepku
+ * for remote tank control (two motor with magnetic coding)
+ * 
+ */
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+WiFiMulti WiFiMulti;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("movduino.ino    created on 2018/6/13 by wuyuepku");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
